61bfc51e56673fb682f46f01cc556df80a04251f
Papa-het <yerkinmm@gmail.com>
2020-10-24 15:58:22 +0600

    feat: prettierrc

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <MainPage />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Component, Vue } from 'vue-property-decorator';
+import MainPage from './views/MainPage.vue';
 
 @Component({
   components: {
-    HelloWorld
-  }
+    MainPage,
+  },
 })
 export default class App extends Vue {}
 </script>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    main page
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'MainPage',
+})
+export default class MainPage extends Vue {}
+</script>
+
+<style></style>
